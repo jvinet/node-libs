ae555a9f9136f4e99d53808ae516ce19fe916e08
Judd Vinet <jvinet@zeroflux.org>
2010-03-09 19:56:06 -0400

    works with consecutive calls now

--- mailer.js
+++ mailer.js
@@ -22,8 +22,9 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 */
 
-/* Fixes/updates by Judd Vinet, 2010-02-15 */
-/* Updated for Node 0.1.30 */
+/* Bugfixes by Judd, 2010-02-15 */
+/* Updated for Node 0.1.30, 2010-02-27 */
+/* Updated for Node 0.1.31, 2010-03-09 */
 
 /********* USAGE ***********
 
@@ -40,75 +41,71 @@ OTHER DEALINGS IN THE SOFTWARE.
 var tcp = require('tcp');
 var sys = require('sys');
 
-var email = {
-  send:function (options, cb){
-		var options     = typeof(options)         == "undefined" ? {} : options;
-		options.to      = typeof(options.to)      == "undefined" ? "example@example.com" : options.to;
-		options.from    = typeof(options.from)    == "undefined" ? "noreply@example.com" : options.from;
-		options.subject = typeof(options.subject) == "undefined" ? "no subject" : options.subject;
-		options.body    = typeof(options.body)    == "undefined" ? "" : options.body;	
-			
-		var self = this;
-		var cb = cb || function(){};
+var send = function(options, cb) {
+	var options     = typeof(options)         == "undefined" ? {} : options;
+	options.to      = typeof(options.to)      == "undefined" ? "example@example.com" : options.to;
+	options.from    = typeof(options.from)    == "undefined" ? "noreply@example.com" : options.from;
+	options.subject = typeof(options.subject) == "undefined" ? "no subject" : options.subject;
+	options.body    = typeof(options.body)    == "undefined" ? "" : options.body;	
+		
+	var cb = cb || function(){};
 
-		this.connection = tcp.createConnection(25);
-		this.connection.addListener("connect", function (socket) {
-			self.connection.send("HELO localhost\r\n");
-			self.connection.send("MAIL FROM: " + options.from + "\r\n");
-			self.connection.send("RCPT TO: " + options.to + "\r\n");
-			self.connection.send("DATA\r\n");
-			self.connection.send("From: " + options.from + "\r\n");
-			self.connection.send("To: " + options.to + "\r\n");
-			self.connection.send("Subject: " + options.subject + "\r\n");
-			// don't use this unless you need to, it triggers spam traps
-			//self.connection.send("Content-Type: text/plain\r\n");
-			self.connection.send(email.wordwrap(options.body) + "\r\n");
-			self.connection.send(".\r\n");
-			self.connection.send("QUIT\r\n");
-			self.connection.close();
-		});
+	var conn = tcp.createConnection(25);
+	conn.addListener("connect", function() {
+		conn.write("HELO localhost\r\n");
+		conn.write("MAIL FROM: " + options.from + "\r\n");
+		conn.write("RCPT TO: " + options.to + "\r\n");
+		conn.write("DATA\r\n");
+		conn.write("From: " + options.from + "\r\n");
+		conn.write("To: " + options.to + "\r\n");
+		conn.write("Subject: " + options.subject + "\r\n");
+		// don't use this unless you need to, it triggers spam traps
+		//conn.write("Content-Type: text/plain\r\n");
+		conn.write(wordwrap(options.body) + "\r\n");
+		conn.write(".\r\n");
+		conn.write("QUIT\r\n");
+		conn.close();
+	});
 
-		var output = '';
+	var output = '';
+	conn.addListener("data", function (data) { output += data; });
 
-		this.connection.addListener("receive", function (data) {
-			output += data;
-		});
-		this.connection.addListener("eof", function(){
-			if(email.parseResponse(output)){
-				cb(null);
-			}else{
-				sys.puts(sys.inspect(arguments));
-				cb(true);
-			}
-		});
-	},
-
-  parseResponse:function(data){
-		var success = false;
-		var d = data.split("\r\n");
-		d.forEach(function(itm){
-			// not sure if all MTAs respond with "250 2.0.0" but Sendmail and Postfix do
-			if(/^250 2\.0\.0/.test(itm)) success = true;
-		});
-		return success;
-  },
-  
-  wordwrap:function(str){
-		var m = 80;
-		var b = "\r\n";
-		var c = false;
-		var i, j, l, s, r;
-		str += '';
-		if (m < 1) {
-			return str;
+	conn.addListener("end", function() {
+		if(parseResponse(output)) {
+			cb(null);
+		} else {
+			// error
+			sys.puts("Mailer error: "+sys.inspect(arguments));
+			cb(true);
 		}
-		for (i = -1, l = (r = str.split(/\r\n|\n|\r/)).length; ++i < l; r[i] += s) {
-			for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : "")){
-				j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
-			}
-		}
-		return r.join("\n");
+	});
+};
+
+var parseResponse = function(data) {
+	var success = false;
+	var d = data.split("\r\n");
+	d.forEach(function(itm){
+		// not sure if all MTAs respond with "250 2.0.0" but Sendmail and Postfix do
+		if(/^250 2\.0\.0/.test(itm)) success = true;
+	});
+	return success;
+};
+
+var wordwrap = function(str) {
+	var m = 80;
+	var b = "\r\n";
+	var c = false;
+	var i, j, l, s, r;
+	str += '';
+	if (m < 1) {
+		return str;
 	}
+	for (i = -1, l = (r = str.split(/\r\n|\n|\r/)).length; ++i < l; r[i] += s) {
+		for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : "")){
+			j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
+		}
+	}
+	return r.join("\n");
 }
 
-exports.send = email.send;
+exports.send = send;
